8775ca6371d5100feabc89cd913f4a10d8fa90cd
sdkjvlh <164035157+sdkjvlh@users.noreply.github.com>
2025-02-20 20:37:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+//--------------nav a--------------------
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.foreach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.foreach(links => {
+                links.classlist.remove('active');
+                document.querySelector('header nav a[href*= ' + id + '] ').classList.add('active');
+            });
+        };
+    });
+};
+
+function handletalk(){
+   var height = document.body.scrollHeight;
+   window.scroll(0,height);
+    
+
+}
+
